9668fc4ec89700d2d942e6a43a1de080fe49d5d1
Ahmed Farido <AhmedFarido@github.com>
2018-03-10 03:56:07 +0200

    first commit

---
+++ Smack/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by FARIDO on 3/10/18.
+//  Copyright © 2018 FARIDO. All rights reserved.
+//
+
+import Foundation
---
+++ Smack/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by FARIDO on 3/10/18.
+//  Copyright © 2018 FARIDO. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
