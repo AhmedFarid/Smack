dac3794d423a85823329f3a819a6a1f04e49e6aa
Ahmed Farido <AhmedFarido@github.com>
2018-03-15 22:17:59 +0200

    xx

---
+++ Smack/messageCell.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class messageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
